aaaae56cb29b395dc08f14ca6f48be4ae1eecf50
Petrov-connect <Petrov.connect@gmail.com>
2021-03-15 11:25:51 +0200

    java code

---
+++ Dictionaries and Hashmaps/src/TwoStrings/Solution.java
@@ -0,0 +1,43 @@
+package TwoStrings;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Solution {
+
+    static String twoStrings(String s1, String s2) {
+
+        Set<Character>set= IntStream.range(0, s1.length()).mapToObj(s1::charAt).collect(Collectors.toSet());
+        return IntStream.range(0, s2.length()).anyMatch(i -> set.contains(s2.charAt(i))) ? "YES" : "NO";
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int qItr = 0; qItr < q; qItr++) {
+            String s1 = scanner.nextLine();
+
+            String s2 = scanner.nextLine();
+
+            String result = twoStrings(s1, s2);
+
+            bufferedWriter.write(result);
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
