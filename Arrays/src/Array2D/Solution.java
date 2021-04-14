a8bd03c6ac184283dc158b5be9e71eccc3d58da7
Petrov-connect <Petrov.connect@gmail.com>
2021-04-14 14:50:35 +0300

    java code

--- Arrays/src/Array2D/Solution.java
+++ Arrays/src/Array2D/Solution.java
@@ -10,7 +10,8 @@ public class Solution {
         int maxSum = Integer.MIN_VALUE;
         for (int i = 0; i < arr.length - 2; i++) {
             for (int j = 0; j < arr.length - 2; j++) {
-                int sum = arr[i][j] + arr[i][j + 1] + arr[i][j + 2] + arr[i + 1][j + 1] + arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2];
+                int sum = arr[i][j] + arr[i][j + 1] + arr[i][j + 2] + arr[i + 1][j + 1] + arr[i + 2][j]
+                        + arr[i + 2][j + 1] + arr[i + 2][j + 2];
                 if (sum > maxSum) {
                     maxSum = sum;
                 }
