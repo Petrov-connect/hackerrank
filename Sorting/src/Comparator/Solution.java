3770201e38fe2c9a1b0519ad4274d08e50b7cbf6
Petrov-connect <Petrov.connect@gmail.com>
2021-04-17 13:00:51 +0300

    java code

---
+++ Sorting/src/Comparator/Solution.java
@@ -0,0 +1,47 @@
+package Comparator;
+//created by J.M.
+
+import java.util.*;
+
+class Player {
+    String name;
+    int score;
+
+    Player(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+}
+
+class Checker implements Comparator<Player> {
+
+    public int compare(Player a, Player b) {
+        int sort = Integer.compare(b.score, a.score);
+        if (sort == 0) {
+            sort = a.name.compareTo(b.name);
+        }
+        return sort;
+    }
+}
+
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+        Player[] player = new Player[n];
+        Checker checker = new Checker();
+
+        for (int i = 0; i < n; i++) {
+            player[i] = new Player(scan.next(), scan.nextInt());
+        }
+        scan.close();
+
+        Arrays.sort(player, checker);
+        for (Player value : player) {
+            System.out.printf("%s %s\n", value.name, value.score);
+        }
+    }
+}
