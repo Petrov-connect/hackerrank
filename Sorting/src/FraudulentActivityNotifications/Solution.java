03fad1681427831d1dd58c532208506cfae8f643
Petrov-connect <Petrov.connect@gmail.com>
2021-04-20 14:36:41 +0300

    java code

--- Sorting/src/FraudulentActivityNotifications/Solution.java
+++ Sorting/src/FraudulentActivityNotifications/Solution.java
@@ -1,61 +1,38 @@
 package FraudulentActivityNotifications;
 //created by J.M.
 
-import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Solution {
 
     static int activityNotifications(int[] expenditure, int d) {
-
-        if (expenditure.length == d) {
-            return 0;
-        }
         int notifications = 0;
+
+        List<Integer> temp = IntStream.of(expenditure).limit(d).boxed().sorted().collect(Collectors.toList());
 
         for (int i = d; i < expenditure.length; i++) {
 
-            int[] num= Arrays.stream(Arrays.copyOfRange(expenditure,i-d,i)).sorted().toArray();
-            double median = (d % 2 == 0)
-                    ? (num[d / 2] + num[(d / 2) - 1]) / 2.0
-                    : num[d / 2];
+            double median = d % 2 != 0
+                    ? temp.get(d/2)
+                    : (temp.get(d/2) + temp.get((d/2) - 1)) / 2.0;
 
             if (expenditure[i] >= median * 2) {
                 notifications++;
             }
+            temp.remove(Collections.binarySearch(temp, expenditure[i - d]));
+            int tempIndex = Collections.binarySearch(temp, expenditure[i]);
+            if (tempIndex < 0) {
+                tempIndex = (-1 * tempIndex) -1;
+            }
+            temp.add(tempIndex, expenditure[i]);
         }
+
         return notifications;
     }
-
-   /* static int activityNotifications(int[] expenditure, int d) {
-        int[] counts = new int[201];
-        double midpoint = (d - 1) / 2.0;
-        int notifications = 0;
-        for (int i = 0; i < expenditure.length; i++) {
-            counts[expenditure[i]]++;
-            if (i >= d) {
-                Integer medianLeft = null;
-                Integer medianRight = null;
-                for (int j = 0, sum = 0;
-                     medianLeft == null || medianRight == null;
-                     j++) {
-                    sum += counts[j];
-                    if (sum > (int)Math.floor(midpoint) && medianLeft == null) {
-                        medianLeft = j;
-                    }
-                    if (sum > (int)Math.ceil(midpoint) && medianRight == null) {
-                        medianRight = j;
-                    }
-                }
-                if (expenditure[i] >= medianLeft + medianRight) {
-                    notifications++;
-                }
-                counts[expenditure[i - d]]--;
-            }
-        }
-        return notifications;
-    }*/
-
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
